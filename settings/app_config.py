6303dcdcb8c946b6ce58c39a401f9d754441d626
Garret47 <abahmetov10@gmail.com>
2025-02-21 12:08:27 +0600

    Convert logging config from JSON to YAML

--- settings/app_config.py
+++ settings/app_config.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass, field
-from pathlib import Path
 import logging.config
-import json
+
+from utils import ConfigReader
 
 
 class AppConfigError(Exception):
@@ -15,18 +15,13 @@ class GUIConfig:
 
 @dataclass
 class AppConfig:
-    FILE_LOGGING_CONFIG: str = 'data/logging.json'
+    FILE_LOGGING_CONFIG: str = 'data/logging.yaml'
     LOGGER_NAME: str = 'console'
     gui: GUIConfig = field(default_factory=GUIConfig)
 
     def configure_logging(self):
-        logging_file = Path(self.FILE_LOGGING_CONFIG)
-        if logging_file.exists():
-            with open(logging_file, 'r') as f:
-                LOGGING_CONFIG = json.load(f)
-                logging.config.dictConfig(LOGGING_CONFIG)
-        else:
-            raise AppConfigError(f'file {self.FILE_LOGGING_CONFIG} not exists')
+        LOGGING_CONFIG = ConfigReader.read(self.FILE_LOGGING_CONFIG)
+        logging.config.dictConfig(LOGGING_CONFIG)
 
 
 app_config = AppConfig()
